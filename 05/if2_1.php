29e94afe5df0914ef9b6dcda1e23174aa70ed672
ogataka55 <ogawatakahiro2107@yahoo.co.jp>
2022-02-23 18:55:28 +0900

    制御構文2の課題(再)

--- 05/if2_1.php
+++ 05/if2_1.php
@@ -8,7 +8,7 @@ echo "あなたの所持金は{$poket_money}円です。<br>";
 
 while ($fund_raising < $poket_money) {
     echo "{$fund_raising}円募金しました。<br>";
-    $poket_money = $poket_money - $fund_raising;
+    $poket_money -= $fund_raising;
     echo "残り残高は{$poket_money}円です。<br>";
 }
 
--- 05/if2_5.php
+++ 05/if2_5.php
@@ -6,19 +6,20 @@ $subjects = ['数学', '英語', '理科', '社会', '国語'];
 foreach ($subjects as $subject) {
     switch ($subject) {
         case '数学':
-            echo '数学の試験は明日です。<br>';
+            $disp = 'の試験は明日です。<br>';
             break;
         case '英語':
-            echo '英語の試験は明後日です。<br>';
+            $disp = 'の試験は明後日です。<br>';
             break;
         case '理科':
-            echo '理科の試験は明々後日です。<br>';
+            $disp = 'の試験は明々後日です。<br>';
             break;
         case '社会':
-            echo '社会の試験は昨日です。<br>';
+            $disp = 'の試験は昨日です。<br>';
             break;
         case '国語':
-            echo '国語の試験は中止です。<br>';
+            $disp = 'の試験は中止です。<br>';
             break;
     }
+    echo "{$subject}{$disp}";
 }
